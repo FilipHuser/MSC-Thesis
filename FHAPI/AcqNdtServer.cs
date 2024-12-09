ad014a1bac06e54a1d6ef2fa985643eac31f5137
Filip Huser <filip.huser2002@gmail.com>
2024-12-09 23:54:45 +0100

    + LoadTemplate method

--- FHAPI/AcqNdtServer.cs
+++ FHAPI/AcqNdtServer.cs
@@ -15,17 +15,56 @@ namespace FHAPI
     }
     internal class AcqNdtServer
     {
-        public AcqNdtServer(string host, string port, TransportType transportType = TransportType.TCP)
+        public AcqNdtServer(string host, string port, TransportType transportType = TransportType.TCP , int dataConnectionTimeout = 2)
         {
             Host = host;
             Port = port;
+            DataConnectionTimeout = dataConnectionTimeout;
             RPCP = XmlRpcProxyGen.Create<IXmlRpcProxy>();
             ((XmlRpcClientProtocol)RPCP).Url = RPCServerURL;
+
+
+            FullList = RPCP.SystemListMethods().ToList();
+            FullList.ForEach(x =>
+            {
+                if (x.StartsWith(ACKRPCNamespace))
+                {
+                    MethodsList.Add(x.Replace(ACKRPCNamespace, ""));
+                }
+                else
+                {
+                    MethodsList.Add(x);
+                }
+            });
         }
+        #region PROPERTIES
         public string? Host { get; set; }
         public string? Port { get; set; }
         public string? RPCServerURL => $"http://{Host}:{Port}/RPC2";
         public TransportType TransportType { get; set; }
         public IXmlRpcProxy RPCP { get; set; }
+        public string? ACKRPCNamespace { get; set; } = "acq.";
+        public List<string> FullList { get; set; } = new List<string>();
+        public List<string> MethodsList { get; set; } = new List<string>();
+        public int DataConnectionTimeout { get; set; }
+        #endregion
+        #region METHODS
+        public void LoadTemplate(string filename)
+        {
+            string data = "";
+            int bufferSize = 1024;
+            byte[] buffer = new byte[bufferSize];
+
+            using (var fs = new FileStream(filename , FileMode.Open , FileAccess.Read))
+            {
+                int bytesRead;
+                while ((bytesRead = fs.Read(buffer, 0, buffer.Length)) > 0)
+                {
+                    string partial = System.Text.Encoding.UTF8.GetString(buffer, 0, bytesRead);
+                    data += partial;
+                }
+            }
+        }
+        #endregion
     }
 }
