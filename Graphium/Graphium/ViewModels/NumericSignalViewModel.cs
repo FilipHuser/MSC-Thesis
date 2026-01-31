b1eaa5a76e2fb3af42e9a651b28801ef7e37b66d
FilipHuser <filip.huser2002@gmail.com>
2026-01-31 16:52:09 +0100

    initial commit for refactorization

---
+++ Graphium/Graphium/ViewModels/NumericSignalViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Graphium.ViewModels
+{
+    internal class NumericSignalViewModel : ViewModelBase
+    {
+        #region PROPERTIES
+
+        #endregion
+        #region METHODS
+        #endregion
+    }
+}
